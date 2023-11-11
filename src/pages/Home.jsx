78f2e24a41f1960756ca1352a707367e0768a277
tarekibnkhayer <tarekibnkhayer@gmail.com>
2023-11-11 17:01:35 +0600

    [build] -create a swipe for  food category

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../components/forHomePage/Banner";
+import Category from "../components/forHomePage/Category";
 
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <Category></Category>
         </div>
     );
 };
